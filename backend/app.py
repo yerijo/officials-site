8368ead22bbf88468119c3af4be25e7059d85dfc
Euie Son <sonchristina@utexas.edu>
2023-08-03 06:11:12 -0500

    edit api route

--- backend/app.py
+++ backend/app.py
@@ -1,36 +1,58 @@
 from flask import Flask, request
 import flask
 import json
+import requests
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+	return "Hello, World!"
 
 @app.route('/users', methods=["GET", "POST"])
 def users():
-    print("users endpoint reached...")
-    if request.method == "GET":
-        with open("users.json", "r") as f:
-            data = json.load(f)
-            data.append({
-                "username": "user4",
-                "pets": ["hamster"]
-            })
+	print("users endpoint reached...")
+	if request.method == "GET":
+		with open("users.json", "r") as f:
+			data = json.load(f)
+			data.append({
+				"username": "user4",
+				"pets": ["hamster"]
+			})
 
-            return flask.jsonify(data)
-    if request.method == "POST":
-        received_data = request.get_json()
-        print(f"received data: {received_data}")
-        message = received_data['data']
-        return_data = {
-            "status": "success",
-            "message": f"received: {message}"
-        }
-        return flask.Response(response=json.dumps(return_data), status=201)
+			return flask.jsonify(data)
+	if request.method == "POST":
+		received_data = request.get_json()
+		print(f"received data: {received_data}")
+		message = received_data['data']
+		return_data = {
+			"status": "success",
+			"message": f"received: {message}"
+		}
+		return flask.Response(response=json.dumps(return_data), status=201)
+
+@app.route('/api', methods=["GET", "POST"])
+def api():
+	print("api endpoint reached...")
+	API_KEY = os.environ.get("CICERO_API")
+	url = "https://cicero.azavea.com/v3.1/official?search_loc=%s&key=%s" % (address, API_KEY)
+	if request.method == "GET":
+		return flask.jsonify(response)
+	if request.method == "POST":
+		received_data = request.get_json()
+		print(f"received data: {received_data}")
+		message = received_data['data']
+		return_data = {
+			"status": "success",
+			"message": f"received: {message}"
+		}
+		address = received_data['data']
+		response = requests.get(url)
+
+		return flask.Response(response=json.dumps(return_data), status=201)
 
 if __name__ == "__main__":
-    app.run("localhost", 6969)
+	app.run("localhost", 6969)
